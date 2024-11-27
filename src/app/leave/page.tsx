e140da2af58f1b4333b9f8c9de4fcc87d4986323
juber <juber.nowawave@gmail.com>
2024-11-27 09:58:43 +0530

    calendar

---
+++ src/app/leave/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLaout"
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb"
+function Leave(){ 
+    
+return(
+    <div>
+     <DefaultLayout >
+      <Breadcrumb pageName="leaves" />
+     </DefaultLayout >
+    </div>
+  )
+}
+
+export default Leave;
